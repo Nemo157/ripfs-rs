a7320e920a01bcbafe057393261ef7db91a6f39f
Wim Looman <wim@nemo157.com>
2017-09-21 20:49:06 +0200

    Log final swarm

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ extern crate bytes;
 mod identity;
 
 use std::str::FromStr;
+use std::time::Duration;
+
 use maddr::{MultiAddr, Segment};
 use libp2p::{ PeerInfo, Swarm };
 use libp2p::identity::HostId;
-use tokio_core::reactor::Core;
+use tokio_core::reactor::{Core, Timeout};
 use futures::{Future, Sink, Stream};
+use futures::future::Either;
 use tokio_io::codec::Framed;
 
 use identity::Identity;
@@ -22,15 +25,15 @@ fn main() {
         include_bytes!("private_key.der").as_ref().to_owned(),
         include_bytes!("public_key.der").as_ref().to_owned()).unwrap();
 
-    println!("host: {:?}", host);
+    println!("host: {:#?}", host);
 
     let arg = std::env::args().skip(1).next();
-    println!("arg: {:?}", arg);
+    println!("arg: {:#?}", arg);
 
     let mut core = Core::new().unwrap();
 
-    fn ping(mut core: Core, mut swarm: Swarm, peer: PeerInfo) {
-        println!("going to ping {:?}", peer);
+    fn ping(core: &mut Core, mut swarm: Swarm, peer: PeerInfo) {
+        println!("going to ping {:#?}", peer);
         swarm.add_peers(vec![peer.clone()]);
         println!("added peer");
         let id = peer.id().clone();
@@ -39,26 +42,34 @@ fn main() {
         let stream = Framed::from_parts(parts, Identity);
         let stream = core.run(stream.send(b"1234567890ABCDEF1234567890ABCDEF"[..].into())).unwrap();
         println!("sent ping");
-        let (result, _stream) = core.run(stream.into_future()).unwrap();
-        println!("ping result {:?}", result.map(|b| String::from_utf8(b.to_vec())));
+        let (result, _) = core.run(stream.into_future()).unwrap();
+        let result = result.unwrap();
+        println!("ping result {:#?}", String::from_utf8(result.to_vec()).unwrap());
     }
 
     if arg == Some("listen".to_owned()) {
         let addr = MultiAddr::from_str("/ip4/127.0.0.1/tcp/4002").unwrap();
         println!("listening at: {}", addr.clone() + Segment::Ipfs(host.hash().clone()));
         let swarm = Swarm::new(host, "ripfs/0.0.0".to_owned(), vec![addr], core.handle()).unwrap();
-        println!("result: {:?}", core.run(swarm));
+        println!("result: {:#?}", core.run(swarm));
     } else if arg == Some("self".to_owned()) {
         let swarm = Swarm::new(host, "ripfs/0.0.0".to_owned(), vec![], core.handle()).unwrap();
-        core.handle().spawn(swarm.clone().map_err(|err| println!("Swarm error {:?}", err)));
+        core.handle().spawn(swarm.clone().map_err(|err| println!("Swarm error {:#?}", err)));
         let addr = MultiAddr::from_str("/ip4/127.0.0.1/tcp/4002/ipfs/QmdxDB3FVu9JoUtSgaVTaCQpswH7ghfRsM3wGYWFSqW7Gb").unwrap();
         let peer = PeerInfo::from_addr(addr).unwrap();
-        ping(core, swarm, peer);
+        ping(&mut core, swarm, peer);
     } else {
         let swarm = Swarm::new(host, "ripfs/0.0.0".to_owned(), vec![], core.handle()).unwrap();
-        core.handle().spawn(swarm.clone().map_err(|err| println!("Swarm error {:?}", err)));
+        core.handle().spawn(swarm.clone().map_err(|err| println!("Swarm error {:#?}", err)));
         let addr = MultiAddr::from_str("/ip4/127.0.0.1/tcp/4001/ipfs/QmcD3Pzo3kwvuZYNcxwEbefhmhR8s2ftd7zMkAWBwMhjax").unwrap();
         let peer = PeerInfo::from_addr(addr).unwrap();
-        ping(core, swarm, peer);
+        ping(&mut core, swarm.clone(), peer);
+        println!("Running swarm for 5 seconds");
+        let timeout = Timeout::new(Duration::from_secs(5), &core.handle()).unwrap();
+        let result = core.run(swarm.select2(timeout)).map_err(|_| ()).unwrap();
+        match result {
+            Either::A((swarm, _)) => println!("Swarm ended: {:#?}", swarm),
+            Either::B((_, swarm)) => println!("Swarm still running: {:#?}", swarm),
+        }
     }
 }
